6beee4137f59fa9f75b6b4eaa72c4a20ece0081c
benjamin <benjamin.guyon@ece.edu.fr>
2022-11-08 11:56:13 +0100

    comit test

--- app/components/header.js
+++ app/components/header.js
@@ -5,10 +5,10 @@ export default function Header() {
 <div className={styles.Back} >
 
       <ul className={styles.NavBar}>
-        <li className={styles.li}><a className={styles.lia} href="/">Home</a></li>
-        <li className={styles.li}><a className={styles.lia} href="/about">About</a></li>
-        <li className={styles.li}><a className={styles.lia} href="/contact">Contact</a></li>
-        <li className={styles.li}><a className={styles.lia} href="/courses">Courses</a></li>
+        <li className={styles.li}><a className={styles.lia} href="/">Acceuil</a></li>
+        <li className={styles.li}><a className={styles.lia} href="/about">A propos</a></li>
+        <li className={styles.li}><a className={styles.lia} href="/contact">Nous contacter</a></li>
+        <li className={styles.li}><a className={styles.lia} href="/courses">Cours</a></li>
 
       </ul>
 
